8a2fa7170ed6cc363e00ddd610350bee7a23e6e9
zonghua2016 <2280314971@qq.com>
2019-12-25 11:41:10 +0800

    增加备案信息

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
+    <div style="width:1000px;margin:20px auto 0; padding:20px 0;">
+      <a target="_blank"
+         href="http://beian.miit.gov.cn/"
+         style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
+        <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">京ICP备19043286号</p>
+      </a>
+      <a target="_blank"
+         href="http://beian.miit.gov.cn/"
+         style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
+        <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">京ICP备19043286号-1</p>
+      </a>
+      <a target="_blank"
+         href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11010502038975"
+         style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="~imgs/blogo.png"
+             style="float:left;" />
+        <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">京公网安备 11010502038975号</p>
+      </a>
+    </div>
+
   </div>
 </template>
 
